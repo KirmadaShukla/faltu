088b8d4dc03c6d12e79e6dcd60d2183b60460bc6
Pranjal Shukla <kirmadashukla@gmail.com>
2025-06-13 12:57:03 +0530

    Update src/layouts/MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import Nav from '../components/Nav';
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col" data-source="src/layouts/MainLayout.jsx">
-      <Header />
-      <main className="flex-grow">
-        {children}
-      </main>
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-white shadow-sm editable" data-source="src/Components/Header.jsx" contentEditable="true" style={{ outline: 'transparent dashed 1px' }}>
+        <div className="max-w-6xl mx-auto px-4 editable" contentEditable="true" style={{ outline: 'transparent dashed 1px' }}>
+          <div className="flex justify-between items-center h-16 editable" contentEditable="true" style={{ outline: 'transparent dashed 1px' }}>
+            <a className="text-2xl font-bold text-gray-900 editable" href="/" data-discover="true" contentEditable="true" style={{ outline: 'transparent dashed 1px' }}>
+              Your Brand
+            </a>
+            <Nav />
+          </div>
+        </div>
+      </header>
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
 };
 
-export default MainLayout; 
+export default MainLayout;
