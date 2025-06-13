b0a7cdae70f0ebd6afc61662a7931d55be0221de
Pranjal Shukla <kirmadashukla@gmail.com>
2025-06-13 12:58:06 +0530

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
+import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
+      <div className="App">
+        <Navbar />
+        <main>
+          <a class="text-2xl font-bold text-gray-900">Your Brand</a> {/* Added link */}
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
